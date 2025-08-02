3e8ffde47aaaddceabd02ff8482cd498aa92b3b0
jv-victtor <victor.costa1@escolar.ifrn.edu.br>
2025-08-02 19:09:18 -0300

    imagens e borda

--- website/views.py
+++ website/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 
 elenco = {
     'atletas': [
-        {'foto': 'images/ferrerinha.jpg', 'nome': 'Ferreirinha', 'idade': 29, 'posicao': 'Atacante', 'nascimento': 'argentina'},
-        {'foto': 'images/leo ortiz.jpg', 'nome': 'léo ortiz', 'idade': 29, 'posicao': 'zagueiro', 'nascimento': 'brasil'},
-        {'foto': 'images/leo pereira.jpg', 'nome': 'léo pereira', 'idade': 29, 'posicao': 'zagueiro', 'nascimento': 'brasil'},
-        {'foto': 'images/alex sandro.png', 'nome': 'alex sandro', 'idade': 34, 'posicao': 'lateral esquerdo', 'nascimento': 'brasil'},
-        {'foto': 'images/wesley.png', 'nome': 'wesley frança', 'idade': 21, 'posicao': 'lateral direito', 'nascimento': 'brasil'},
-        {'foto': 'images/erick.png', 'nome': 'erick pulgar', 'idade': 31, 'posicao': 'volante', 'nascimento': 'chile'},
-        {'foto': 'images/jorginho.png', 'nome': 'jorginho', 'idade': 33, 'posicao': 'volante', 'nascimento': 'itália'},
-        {'foto': 'images/arrascaeta.png', 'nome': 'de arrascaeta', 'idade': 31, 'posicao': 'meia atacante', 'nascimento': 'uruguai'},
-        {'foto': 'images/luiz.png', 'nome': 'luiz araújo', 'idade': 29, 'posicao': 'ponta direita', 'nascimento': 'brasil'},
-        {'foto': 'images/bh.png', 'nome': 'bruno henrique', 'idade': 34, 'posicao': 'ponta esquerda', 'nascimento': 'brasil'},
-        {'foto': 'images/pedro.png', 'nome': 'pedro', 'idade': 28, 'posicao': 'centroavante', 'nascimento': 'brasil'}
+        {'foto': 'images/rafael.jpg', 'nome': 'Rafael', 'idade': 36, 'posicao': 'Goleiro', 'nascimento': 'Brasil'},
+        {'foto': 'images/arboleda.jpg', 'nome': 'Robert Arboleda', 'idade': 33, 'posicao': 'Zagueiro', 'nascimento': 'Equador'},
+        {'foto': 'images/alan_franco.png', 'nome': 'Alan Franco', 'idade': 28, 'posicao': 'Zagueiro', 'nascimento': 'Argentina'},
+        {'foto': 'images/wendell.jpg', 'nome': 'Wendell', 'idade': 32, 'posicao': 'Lateral Esquerdo', 'nascimento': 'Brasil'},
+        {'foto': 'images/cedric.webp', 'nome': 'Cédric Soares', 'idade': 33, 'posicao': 'Lateral Direito', 'nascimento': 'Portugal'},
+        {'foto': 'images/luiz_gustavo.webp', 'nome': 'Luiz Gustavo', 'idade': 37, 'posicao': 'Volante', 'nascimento': 'Brasil'},
+        {'foto': 'images/damian_bobadilla.webp', 'nome': 'Damián Bobadilla', 'idade': 24, 'posicao': 'Volante', 'nascimento': 'Paraguai'},
+        {'foto': 'images/lucas_moura.webp', 'nome': 'Lucas Moura', 'idade': 33, 'posicao': 'Meia Atacante', 'nascimento': 'Brasil'},
+        {'foto': 'images/ferrerinha.jpg', 'nome': 'Ferreirinha', 'idade': 27, 'posicao': 'Ponta Esquerda', 'nascimento': 'Brasil'},
+        {'foto': 'images/lucas_ferreira.jpg', 'nome': 'Lucas Ferreira', 'idade': 19, 'posicao': 'Ponta Direita', 'nascimento': 'Brasil'},
+        {'foto': 'images/andre_silva.webp', 'nome': 'André Silva', 'idade': 26, 'posicao': 'Centroavante', 'nascimento': 'Brasil'}
     ]
 }
 
 info_site = {
-    'titulo': 'clubes de regatas do flamengo',
-    'historia': 'o flamengo foi fundado em 17 de novembro de 1895 para as disputas de remo. A entrada da equipe de futebol só aconteceu em 1912.',
+    'titulo': 'São Paulo Futebol Clube',
+    'historia': 'O São Paulo Futebol Clube foi fundado em 25 de janeiro de 1930, fruto da união de dissidentes do Club Athletico Paulistano e da Associação Athlética das Palmeiras. A ata de fundação foi assinada na madrugada do dia seguinte (26 de janeiro), mas oficialmente o clube considera como data magna o dia 25, em homenagem ao aniversário da cidade de São Paulo.',
     'autores': ['João Victor e Elias Renner']
 }
 
